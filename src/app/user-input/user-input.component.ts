3a415ec00c416ff1121c37357c42a39294e5d211
Nithushan J. <UT01268@unicomtic.com>
2024-10-21 16:47:42 +0530

    ngmodel set up two way binding  78

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-
+  enterInitialInvestment = '0';
+  enterAnnualInvestment='0';
+  enterExpectedReturn='0';
+  enterDuration='5';
+  onsubmit(){
+    console.log('hello')
+    console.log(this.enterInitialInvestment)
+    console.log(this.enterAnnualInvestment)
+    console.log(this.enterExpectedReturn)
+    console.log(this.enterDuration)
+  }
 }
