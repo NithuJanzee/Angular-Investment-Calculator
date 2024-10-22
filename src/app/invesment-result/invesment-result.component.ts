2ac05856925fbf517bb8af18b476aaa25a754dd5
Nithushan Janson <jansonnithusanofficial@gmail.com>
2024-10-22 14:36:23 +0530

    added currency pipe

--- src/app/invesment-result/invesment-result.component.ts
+++ src/app/invesment-result/invesment-result.component.ts
@@ -1,9 +1,10 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-invesment-result',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './invesment-result.component.html',
   styleUrl: './invesment-result.component.css'
 })
